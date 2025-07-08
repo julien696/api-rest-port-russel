0d5ef1894727daaafc783877d338b9fc336c062e
julien <julienchauvin@hotmail.fr>
2025-07-08 20:22:35 +0200

    vercel

--- api/index.js
+++ api/index.js
@@ -10,14 +10,14 @@ const serverless = require('serverless-http');
 connectToMongo();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views')); 
+app.set('views', path.join(process.cwd(), 'views')); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/docs', express.static(path.join(__dirname, '../docs')));
+app.use('/docs', express.static(path.join(process.cwd(), 'docs')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
