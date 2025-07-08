984597395f2547e9d09134f0a560c5ceab73e9eb
julien <julienchauvin@hotmail.fr>
2025-07-08 19:25:38 +0200

    vercel

---
+++ api/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express'); 
+const connectToMongo = require('../services/mongoConnect')
+const app = express(); 
+const port = process.env.PORT;
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('../routes/index');
+const serverless = require('serverless-http');
+
+connectToMongo();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/docs', express.static(path.join(__dirname, 'docs')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
+ 
